55fca83f3e1dadc517164c476509cc17132d1ff2
Julio Romero <jromeroacive@gmail.com>
2022-07-15 05:10:22 -0430

    Se adapto el proyecto a dispositivos moviles

---
+++ Main.js
@@ -0,0 +1,38 @@
+//Selectores
+const sumit = document.querySelector(".Sumit");
+const container = document.querySelector(".Container");
+const Cal = document.querySelector(".Calificacion");
+let Button = document.querySelector("ul").getElementsByTagName("Button");
+let n = document.querySelector(".number");
+
+
+
+//Recorre las ul donde se ubican los botones que el usuario pulsa
+
+[].forEach.call(Button, btn => {
+    btn.addEventListener('click', () => {
+      
+     n.innerHTML = btn.innerHTML;
+     
+     btn.style.color = "orange";
+    btn.style.background = "white"
+    });
+  });
+
+
+//Muestra y oculta el contenido de la pagina 
+
+sumit.addEventListener("click",() =>{
+    
+container.style.display = "none";
+ Cal.style.display = "block";
+});
+       
+    
+
+
+
+
+
+
+
